03a67f08b12d6053b9a4f8129bc030a8b7d38b48
obscuredusername <zubayrr1011@gmail.com>
2025-04-14 00:58:51 +0500

    minor fixes

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bazaarpakistan.up.railway.app","*"], 
+    allow_origins=[
+        "https://bazaarpakistan.up.railway.app",
+        "https://productsbackendbazaar-production.up.railway.app",
+        # Add any other frontend URLs that might be accessing your API
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
